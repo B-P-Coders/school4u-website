80d8765a5456e7c638cf433595f16b5b4975e538
n00sek <n00sek@onet.pl>
2022-03-16 22:18:31 +0100

    a

--- school4u-website/src/App.jsx
+++ school4u-website/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './images/logo.svg'
+import './styles/App.css'
 import { Layout } from './components/Layout'
 
 function App() {
