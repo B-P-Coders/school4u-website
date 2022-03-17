654abc01483bf0d71acdafdf97b81b71e8748ee2
n00sek <74304320+n00sek@users.noreply.github.com>
2022-03-17 08:49:33 +0100

    a

--- school4u-website/src/App.jsx
+++ school4u-website/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <div>
         <header className="header">
           <div className="links">
+            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.2" className="github" target="_blank"><i class="fa-brands fa-github"></i> Github</a></p>
             <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/Schoo4u-win32-ia32.zip" download className="windows" target="_blank"><i class="fa-brands fa-windows "></i> Windows</a></p>
             <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/school4u-darwin-x64.zip" download className="macos" target="_blank"><i class="fa-brands fa-apple"></i> Mac OS</a></p>
             <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/Schoo4u-linux-x64.zip" download className="linux" target="_blank"><i class="fa-brands fa-linux"></i> Linux</a></p>
