67a05aef4a46bae3004b560574bf6defb4c5e956
piotreknow02 <65082017+piotreknow02@users.noreply.github.com>
2022-03-29 23:49:34 +0200

    1.3

--- school4u-website/src/App.jsx
+++ school4u-website/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
       <div>
         <header className="header">
           <div className="links">
-            <p><a href="https://github.com/B-P-Coders/coding-night/releases/tag/1.2" className="github" target="_blank"><i class="fa-brands fa-github"></i> Github</a></p>
-            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/Schoo4u-win32-ia32.zip" download className="windows" target="_blank"><i class="fa-brands fa-windows "></i> Windows</a></p>
-            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/school4u-darwin-x64.zip" download className="macos" target="_blank"><i class="fa-brands fa-apple"></i> Mac OS</a></p>
-            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.1/Schoo4u-linux-x64.zip" download className="linux" target="_blank"><i class="fa-brands fa-linux"></i> Linux</a></p>
+            <p><a href="https://github.com/B-P-Coders/coding-night/releases/tag/1.3" className="github" target="_blank"><i class="fa-brands fa-github"></i> Github</a></p>
+            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.3/Schoo4u-win32-ia32.zip" download className="windows" target="_blank"><i class="fa-brands fa-windows "></i> Windows</a></p>
+            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.3/school4u-darwin-x64.zip" download className="macos" target="_blank"><i class="fa-brands fa-apple"></i> Mac OS</a></p>
+            <p><a href="https://github.com/B-P-Coders/coding-night/releases/download/1.3/Schoo4u-linux-x64.zip" download className="linux" target="_blank"><i class="fa-brands fa-linux"></i> Linux</a></p>
           </div>
           <img src={laptop} className="laptop" alt="laptop" />
           <img src={logo} className="logo" alt="logo" />
